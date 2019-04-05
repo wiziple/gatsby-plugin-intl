8916773ef2891b68f9836af4ea1899e544fff48e
wiziple <wiziple@gmail.com>
2019-04-04 20:03:43 -0700

    Update gatsby-starter-default-intl

--- examples/gatsby-starter-default-intl/src/components/redirect.js
+++ examples/gatsby-starter-default-intl/src/components/redirect.js
@@ -3,7 +3,7 @@ import { injectIntl } from "gatsby-plugin-intl"
 import SEO from "../components/seo"
 
 const Redirect = ({ intl }) => {
-  return <SEO title={`404: ${intl.formatMessage({ id: "title" })}`} />
+  return <SEO title={`${intl.formatMessage({ id: "title" })}`} />
 }
 
 export default injectIntl(Redirect)
