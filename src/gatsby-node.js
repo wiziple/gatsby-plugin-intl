37ac60d6be1acb078464e305cd234e7f520b385b
davejs136 <davejs136@gmail.com>
2020-02-07 10:39:30 -0400

    fix: deleted flattenMessages

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,21 +1,6 @@
 const webpack = require("webpack")
 const { createJson, makeQuery } = require("./handle")
 
-function flattenMessages(nestedMessages, prefix = "") {
-  return Object.keys(nestedMessages).reduce((messages, key) => {
-    let value = nestedMessages[key]
-    let prefixedKey = prefix ? `${prefix}.${key}` : key
-
-    if (typeof value === "string") {
-      messages[prefixedKey] = value
-    } else {
-      Object.assign(messages, flattenMessages(value, prefixedKey))
-    }
-
-    return messages
-  }, {})
-}
-
 exports.onCreateWebpackConfig = ({ actions, plugins }, pluginOptions) => {
   const { redirectComponent = null, languages, defaultLanguage } = pluginOptions
   if (!languages.includes(defaultLanguage)) {
