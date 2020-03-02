340971c57fdee203fc615b604a4ab42fe815ac28
Judith Hartmann <judith.hartmann@futurice.com>
2020-03-02 18:37:11 +0100

    Fix ts errors in strict mode
     Generic type 'Provider' requires 1 type argument(s).
    Generic type 'Consumer' requires 1 type argument(s).

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,6 @@ declare module 'gatsby-plugin-intl' {
   export class Link<TState> extends gatsby.Link<TState> {}
   export const navigate: typeof gatsby.navigate;
   export const changeLocale: (language: string, to?: string) => void;
-  export const IntlContextProvider: React.Provider;
-  export const IntlContextConsumer: React.Consumer;
+  export const IntlContextProvider: React.Provider<any>;
+  export const IntlContextConsumer: React.Consumer<any>;
 }
