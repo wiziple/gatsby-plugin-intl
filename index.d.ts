e0682ba5c8b7464c4369a8224c872caa9d0a8fa6
Audun Rundberg <audun@rundberg.no>
2020-07-07 15:00:16 +0200

    flesh out typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,21 @@
-declare module 'gatsby-plugin-intl' {
-  import * as gatsby from 'gatsby';
-  import React from 'react';
+declare module "gatsby-plugin-intl" {
+  import * as gatsby from "gatsby"
+  import React from "react"
 
-  export * from 'react-intl';
+  export * from "react-intl"
 
   export class Link<TState> extends gatsby.Link<TState> {}
-  export const navigate: typeof gatsby.navigate;
-  export const changeLocale: (language: string, to?: string) => void;
-  export const IntlContextProvider: React.Provider;
-  export const IntlContextConsumer: React.Consumer;
+  export const navigate: typeof gatsby.navigate
+  export const changeLocale: (language: string, to?: string) => void
+
+  import { IntlShape } from "react-intl"
+  interface GatsbyPluginIntlShape extends IntlShape {
+    language: string
+    languages: string[]
+    routed: boolean
+    originalPath: string
+    redirect: boolean
+  }
+  export const IntlContextProvider: React.Provider<GatsbyPluginIntlShape>
+  export const IntlContextConsumer: React.Consumer<GatsbyPluginIntlShape>
 }
