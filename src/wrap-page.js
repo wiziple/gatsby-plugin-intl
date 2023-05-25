e67475772ba133ab81dbcdc2e5b96e389c648142
Emil Karlsson <emil.karlsson@teamtailor.com>
2023-05-25 14:47:23 +0200

    Load formatjs locale data from correct path

--- src/wrap-page.js
+++ src/wrap-page.js
@@ -11,12 +11,12 @@ const polyfillIntl = (language) => {
   try {
     if (!Intl.PluralRules) {
       require("@formatjs/intl-pluralrules/polyfill")
-      require(`@formatjs/intl-pluralrules/dist/locale-data/${locale}`)
+      require(`@formatjs/intl-pluralrules/locale-data/${locale}`)
     }
 
     if (!Intl.RelativeTimeFormat) {
       require("@formatjs/intl-relativetimeformat/polyfill")
-      require(`@formatjs/intl-relativetimeformat/dist/locale-data/${locale}`)
+      require(`@formatjs/intl-relativetimeformat/locale-data/${locale}`)
     }
   } catch (e) {
     throw new Error(`Cannot find react-intl/locale-data/${language}`)
